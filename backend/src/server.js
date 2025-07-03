5b51cbce06b36f0181947ee24f0390f3ba8efe13
daksh <daksh.a@somaiya.edu>
2025-07-04 04:48:50 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,33 +1,47 @@
-import express from  "express"
-import notesRoutes from "./routes/notesRoutes.js";
-const app = express();
-import {connectDB} from "./config/db.js";
-import dotenv from "dotenv";
-import ratelimiter from "./middleware/ratelimiter.js";
-//import {Ratelimit} from '@upstash/ratelimit';
-//import {Redis} from '@upstash/redis'; 
+import express from "express";
 import cors from "cors";
-//import ratelimit from "./config/upstash.js"; // Import the rate limiter configuration
+import dotenv from "dotenv";
+import path from "path";
+
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
-app.use(cors({
-  origin:"http://localhost:5173", // Allow requests from the frontend // Allow specific HTTP
-}));
- 
-app.use(express.json()); 
-app.use(ratelimiter); // Apply rate limiting middleware
+
+const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
+
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// });
+
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 connectDB().then(() => {
- app.listen(PORT,() => {
-  console.log("server is listening at port", PORT);
-})    
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
 });
-  
-// Middleware to parse JSON request bodies req.body
-//our simple custom middleware to log requests
-// app.use((req, res, next) => {
-//   console.log(`${req.method} request for '${req.url}'`); // Log the request method and URL
-//   next(); // Proceed to the next middleware or route handler
-// });
